4f4e932887161aed11b0a5b2c46df6fb4465f888
Burma Brown <awsomeisme14@gmail.com>
2015-04-20 13:34:34 -0700

    quiz

---
+++ index.php
@@ -0,0 +1,23 @@
+<<!DOCTYPE html>
+<html>
+<head>
+	<title>
+		XCIV
+	</title>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script> 
+	<script src="main.js"></script> 
+	<link rel="stylesheet" type="text/css" href="main.css">
+	 </head>
+<body>
+	<div class="dan">
+		<p>
+			CLICK ON ME
+		</p>
+	</div>
+	<div id="#iwao">
+		<p>
+			CLICK ON ME
+		</p>
+	</div>
+</body>
+</html>
---
+++ main.js
@@ -0,0 +1,12 @@
+$( document ).ready(function(){
+	 $( 'div' ).click(function() {
+	   alert( "Hey you clicked on me" );
+	});
+   
+});
+
+$( document ).ready(function(){
+	$( '#iwao' ).click(function() {
+	    alert( "Hey you clicked on me again" );
+	});
+});
